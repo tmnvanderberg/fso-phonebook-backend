cc66626f436fe22e331a360450a6c26c45418b56
Timon van der Berg <tmnvanderberg@gmail.com>
2023-11-10 15:39:58 +0100

    3.3

--- index.js
+++ index.js
@@ -31,6 +31,27 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`
+    <p> 
+      Phonebook has info for ${persons.length} people
+    </p> 
+    <p>
+      ${new Date()} 
+    </p>
+    `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
