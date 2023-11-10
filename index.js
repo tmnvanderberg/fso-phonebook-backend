1509ff8ca88f55d415a8d09f33f3232e99add839
Timon van der Berg <tmnvanderberg@gmail.com>
2023-11-10 17:04:57 +0100

    3.5

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ let persons = [
     }
 ]
 
+const generateId = () => {
+  return Math.floor(1000000 * Math.random())
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
@@ -56,6 +60,28 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    console.log(body)
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number || false,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
