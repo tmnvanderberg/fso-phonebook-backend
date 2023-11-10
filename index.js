547b306c083555a767be4879fb9d24711869bd27
Timon van der Berg <tmnvanderberg@gmail.com>
2023-11-10 17:58:01 +0100

    middleware

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const { request } = require('express')
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let persons = [
     { 
@@ -92,6 +101,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
