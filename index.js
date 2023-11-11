458c7a3661fcf5f66d0c09a9b5e87a0a4c07e40a
Timon van der Berg <tmnvanderberg@gmail.com>
2023-11-11 18:20:10 +0100

    3.8

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+morgan.token('data', function (req, res) { 
+  return JSON.stringify(req.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
     { 
